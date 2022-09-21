a97988ea8d38f16a5896c7ccbeafc2fb5463dff9
Yura <Yura_sashin@mail.ru>
2022-09-21 19:39:57 +0300

    рещил задачу 13

---
+++ task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write("Введите целое число: "); //запрсил число
+int a = Convert.ToInt32(Console.ReadLine());// Конвертировал число из текста в int
+if (a < 99)
+{
+    Console.WriteLine($"{a} -> Третьей цифры нет");
+}
+else
+{
+  int RemoveSekondDigit ( int arg) 
+  {
+int firstDigit = arg%1000;
+int lengthDigit = firstDigit/100;
+return lengthDigit;
+  } 
+  int transformation = RemoveSekondDigit(a);
+  Console.WriteLine($"Третье цифра числа{a} -> {transformation} ");
+}
--- task14/Program.cs
+++ task14/Program.cs
@@ -5,11 +5,11 @@
 // 46 -> нет
 // 161 -> да
 
-Console.Write("Введите первое  целое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-bool Мultiple(int arg)
+Console.Write("Введите первое  целое число: ");//Запросил целое число
+int a = Convert.ToInt32(Console.ReadLine());// Конвертировал целое число из текста в int
+bool Мultiple(int arg)//Создал функцию
 {
-    return arg%7 == 0 && arg%23 == 0;
+    return arg%7 == 0 && arg%23 == 0; // Проверяю кратность чила одновремено на 7 и 23
 }
-if(Мultiple(a)) Console.WriteLine($"Число {a} кратно одновременно 7 и 23");
-else Console.WriteLine($"Число {a} НЕ кратно одновременно 7 и 23");
+if(Мultiple(a)) Console.WriteLine($"Число {a} кратно одновременно 7 и 23");//Вывожу ответ, если функия выполняется
+else Console.WriteLine($"Число {a} НЕ кратно одновременно 7 и 23");//Вывожу ответ, если функция не выполняется
