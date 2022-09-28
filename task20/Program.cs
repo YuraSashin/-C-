df024905feabaac62adbd3cebdf8213164ce1291
Yura <Yura_sashin@mail.ru>
2022-09-28 14:31:44 +0300

    Решил задачу 24

--- task20/Program.cs
+++ task20/Program.cs
@@ -19,6 +19,5 @@ double Distan( int argX1 , int argY1, int argX2, int argY2)
     double resault = Math.Sqrt(Math.Pow((argX2 - argX1 ),2)+Math.Pow((argY2 -argY1),2));
     return resault ;
 }
-double resault = Math.Round(Distan(x2,x1,y2,y1))  ;
 double distance = Distan(x1,y1,x2,y2);
 Console.WriteLine(Math.Round(distance,2, MidpointRounding.ToZero));
---
+++ task24/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32( Console.ReadLine());
+int Sumnumbers( int num)
+{
+    int sum = default;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+int sumresult = Sumnumbers(number);
+Console.WriteLine($"Сумма числел от 1 до {number} = {sumresult} ");
