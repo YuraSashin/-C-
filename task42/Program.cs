13f7872ac62a913dde6b2436d723f9925bd30ce0
Yura <Yura_sashin@mail.ru>
2022-10-04 20:35:54 +0300

    решил задачи 40,42

---
+++ task42/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая буде преобразовывать десятичное число в двоичное
+
+Console.Write("Введите первое  число: "); //запросил число
+int a = Convert.ToInt32(Console.ReadLine());// Конвертировал число из текста в int
+
+int TranslationBinarySystem(int num)
+{
+   int binary = 0;
+   int count =1;
+    
+while (num>=1)
+{
+binary= binary+num%2*count;
+count=count*10;
+num=num/2;
+}
+return binary;
+}
+
+int translationBinarySystem = TranslationBinarySystem(a);
+Console.WriteLine($"{a} = {translationBinarySystem}");
