aec06aed6a42de44d1be05237c39170da39c6b55
Yura <Yura_sashin@mail.ru>
2022-10-12 01:40:13 +0300

    Решил задачу 56

---
+++ task56/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)//Метод создания случайного двумерного массива 
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)// matrix.GetLength(0) это колличество строк(rows), индекс показывает измерение массива
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)//matrix.GetLength(1) это колличество столбцов(columns)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)//Метод вывода массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+void PrintArray(int[] array)//Метод вывода массива
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int[] MinSum(int[,] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+    int sum = default;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i,j];
+
+        }
+        array[i] = sum;
+        sum = default;
+    }
+    return array;
+}
+int ArrayMinSum(int[] array)
+{
+    
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        
+    }
+    return min;
+}
+
+
+int[,] array2D = CreateMatrixRndInt(4, 3, 1, 9);
+PrintMatrix(array2D);
+int[] minSumArray = MinSum(array2D);
+int minSum =  ArrayMinSum(minSumArray);
+Console.WriteLine();
+PrintArray(minSumArray);
+Console.WriteLine($"Минимальная сумма = {minSum}");
